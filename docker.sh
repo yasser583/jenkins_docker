8370dd48d7d23fdda5fe5464b891f116228edc2b
yasser583 <yasser583@gmail.com>
2018-04-23 09:04:51 -0500

    Add files via upload

---
+++ docker.sh
@@ -0,0 +1,6 @@
+sudo curl https://get.docker.com | sh
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
